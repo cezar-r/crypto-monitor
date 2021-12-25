ced1d3ff3d0796f2574f8e4ca0158f4233d3f889
cezar-r <59450965+cezar-r@users.noreply.github.com>
2021-12-24 19:20:43 -0700

    Update KuCoinAPI.py

--- src/KuCoinAPI.py
+++ src/KuCoinAPI.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+File containing the KuCoin API object
+"""
+
 from kucoin.client import Client
 
 API_SECRET = "<api_secret>"
@@ -8,8 +14,21 @@ PASSPHRASE = "<passphrase>"
 client = Client(API_KEY, API_SECRET, PASSPHRASE)
 
 class KuCoinAPI:
-
+	"""A class representing the KuCoinAPI"""
 	def get_price(ticker):
+		"""
+		Method that gets the price of a ticker
+		
+		Parameters
+		----------
+		ticker : str
+			Ticker of the given crypto
+		
+		Returns
+		-------
+		price : float
+			price of ticker
+		"""
 		price = client.get_order_book(f'{ticker}-USDT')['bids'][0][0]
 		return price
 
